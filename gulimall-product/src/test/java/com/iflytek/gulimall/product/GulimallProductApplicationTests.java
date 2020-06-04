3ee53bfae8d76ef99b7f9e5179160f65fd8650d9
rclin <1016933282@qq.com>
2020-06-04 09:09:26 +0800

    初始化工程

---
+++ gulimall-product/src/test/java/com/iflytek/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iflytek.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
